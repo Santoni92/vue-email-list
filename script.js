919a5596e066d2caeb0b19fd934dd0919f282d0c
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-04 17:14:56 +0200

    aggiunta il parsing dell'oggetto ritornato

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ const app = new Vue({
     },
     mounted(){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-            //const result = response.data;
-           // console.log(result);
-            //this.email = response.data;
-            this.email = JSON.parse(response.data);
+            let result = response;
+            console.log(result);
+            result = response.data;
+            console.log(result);
+            this.email = JSON.parse('response.data.response');
+
+
+            /*effettuo una richiesta AJAX in javascript (in particolare ricorro alla libreria axios) ad un URL del 
+            server di boolean il quale ritorna un dato in formato JSON . Una volta ricevuto il dato ne faccio il parsing 
+            tramite il metodo parse() in modo da ottenere un oggetto javascript da poter manipolare.
+            'response' è solo un alias che rappresenta l'oggetto (la stringa) in formato JSON */
         });
     }
 });
