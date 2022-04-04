b118965c51c44de76a990e76457af5cd2425ef73
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-04 19:24:37 +0200

    dentro mounted effettuo un ciclo cos' da pushare il contenuto di response presente in data in un array che andrò a renderizzare nell'html tramite v-for

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ console.log('JS OK!');
 const app = new Vue({
     el:'#app',
     data:{
-        email: ''
+        email: '',
+        arrayEmails: []
     },
     mounted(){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-            // let result = response;
-            //console.log(result);
             const result = response.data;
             console.log(result);
-           // this.email = JSON.parse('response.data');
-            //this.email = JSON.parse(result);
             this.email = result.response;
 
-
-            /*effettuo una richiesta AJAX in javascript (in particolare ricorro alla libreria axios) ad un URL del 
-            server di boolean il quale ritorna un dato in formato JSON . Una volta ricevuto il dato ne faccio il parsing 
-            tramite il metodo parse() in modo da ottenere un oggetto javascript da poter manipolare.
-            'response' è solo un alias che rappresenta l'oggetto (la stringa) in formato JSON */
+            /*      BONUS
+            for(let i = 0; i < 10;i++){
+                this.array.push(result.response);
+            }
+            
+            console.log(this.array);
+            */
+           
         });
     }
 });
+
+ /*effettuo una richiesta AJAX in javascript (in particolare ricorro alla libreria axios) ad un URL del 
+ server di boolean il quale ritorna un dato in formato JSON . Una volta ricevuto il dato ne faccio il parsing 
+ tramite il metodo parse() in modo da ottenere un oggetto javascript da poter manipolare.
+ 'response' è solo un alias che rappresenta l'oggetto (la stringa) in formato JSON */
